fec8e26771e9267d8e2a7ceaff5c199b0928e6b6
York Ng <yorkhayng@gmail.com>
2025-03-10 17:35:43 -0400

    silhouette score + MI evaluation

--- create_vectors_masakhanews.py
+++ create_vectors_masakhanews.py
@@ -1,7 +1,6 @@
 #Create vectors for languages in the MasakhaNews and SemRel datasets
 #Run in URIEL++ repo.
-from lang2vec import URIEL2 as uriel
-from lang2vec import lang2vec as l2v
+from urielplus import urielplus as uriel
 import torch
 import numpy as np
 
@@ -12,35 +11,35 @@ new_file_path = 'semrel_vectors\\new\\'
 
 # # #Save
 # # #For old vectors
-# # uriel.vanilla_URIEL()
+# # uriel.reset()
 # # #Use feature_averages.npz for old syntax_average.pt (rename to features.npz)
 # # #Use feature_predictions.npz for old syntax_knn.pt (rename to features.npz)
 
 # # #For new vectors
-# # # uriel.update_URIEL()
-# # # uriel.imputation_interface()
+uriel.integrate_databases()
+uriel.imputation_interface()
 # # #Use average, softImpute for new syntax_average.pt (rename to features.npz)
 # # #Use union, softImpute for new syntax_knn.pt (rename to features.npz)
 
 # # # #Run three times: geo.pt, syntax_knn.pt, syntax_average.pt
 # # #For MasakhaNews
-# # # iso_code_langs = ['amh', 'eng', 'fra', 'hau', 'ibo', 'lin', 'lug', 'orm', 'pcm', 'run', 'sna', 'som', 'swa', 'tir', 'xho', 'yor']
+iso_code_langs = ['amh', 'eng', 'fra', 'hau', 'ibo', 'lin', 'lug', 'orm', 'pcm', 'run', 'sna', 'som', 'swa', 'tir', 'xho', 'yor']
 # #For SemRel
 # iso_code_langs = ["afr", "amh", "arb", "arq", "ary", "eng", "spa", "hau", "hin", "ind", "kin", "mar", "pan", "tel"]
 
 # # # #For old vectors only
-# # vector = l2v.vector("syntactic", iso_code_langs) #Replace "syntactic" with "geographic" for geo.pt. 
+# # vector = uriel.vector("syntactic", iso_code_langs) #Replace "syntactic" with "geographic" for geo.pt. 
 # # vector = {key: [0 if x == -1 else x for x in value] for key, value in vector.items()} #Filling in missing values with 0
 
 # #For new vectors only
 # #For MasakhaNews
-# # glottocode_langs = ['amha1245', 'stan1293', 'stan1290', 'haus1257', 'nucl1417', 'ling1263', 'gand1255', 'west2721', 'nige1257', 'rund1242', 'shon1251', 'soma1255', 'swah1253', 'tigr1271', 'xhos1239', 'yoru1245']
+glottocode_langs = ['amha1245', 'stan1293', 'stan1290', 'haus1257', 'nucl1417', 'ling1263', 'gand1255', 'west2721', 'nige1257', 'rund1242', 'shon1251', 'soma1255', 'swah1253', 'tigr1271', 'xhos1239', 'yoru1245']
 # #For SemRel
 # glottocode_langs = ['afri1274', 'amha1245', 'stan1318', 'alge1239', 'moro1292', 'stan1293', 'stan1288', 'haus1257', 'hind1269', 'indo1316', 'kiny1244', 'mara1378', 'panj1256', 'telu1262']
-# vector = l2v.vector("syntactic", glottocode_langs) #Replace "syntactic" with "geographic" for geo.pt, vice-versa for syntax_knn.pt or syntax_average.pt. 
-# for index, lang in enumerate(iso_code_langs):
-#     vector[lang] = vector[glottocode_langs[index]]
-#     del vector[glottocode_langs[index]]
+vector = uriel.vector("syntactic", glottocode_langs) #Replace "syntactic" with "geographic" for geo.pt, vice-versa for syntax_knn.pt or syntax_average.pt.
+for index, lang in enumerate(iso_code_langs):
+    vector[lang] = vector[glottocode_langs[index]]
+    del vector[glottocode_langs[index]]
 
 # torch.save(vector, new_file_path + 'syntax_knn.pt') #Replace name
 
